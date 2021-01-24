83469b90d100ae70cd1c6811154f249307d6ce20
Bach Vu <pakvu@cs.uchicago.edu>
2021-01-24 23:43:35 +0700

    Add django review summary

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -9,5 +9,6 @@ urlpatterns = [
     # home is a function which is declared in the views.py file
     path('category/', include('api.category.urls')),
     path('product/', include('api.product.urls')),
-    path('user/', include('api.user.urls'))
+    path('user/', include('api.user.urls')),
+    path('order/', include('api.order.urls')),
 ]
